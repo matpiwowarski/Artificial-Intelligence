1a5a9081ea4fcd57fe6bf2b176634987b867092e
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-08 14:10:28 +0100

    new ASP.NET MVC project

---
+++ N_Queens_problem/N_Queens_problem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace N_Queens_problem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
