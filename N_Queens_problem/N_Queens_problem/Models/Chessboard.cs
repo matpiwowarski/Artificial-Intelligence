bc37039275c9bce353d4702fbd0248bfdc2eff25
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-13 12:13:23 +0100

    chessboard array

--- N_Queens_problem/N_Queens_problem/Models/Chessboard.cs
+++ N_Queens_problem/N_Queens_problem/Models/Chessboard.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 namespace N_Queens_problem.Models
 {
+    public enum ChessPiece
+    {
+        Empty,
+        Queen
+    }
+
     public class Chessboard
     {
         public int Size { get; set; }
+        public ChessPiece[,] Board;
 
         public Chessboard(int size)
         {
             Size = size;
+            Board = new ChessPiece[size, size];
         }
     }
 }
