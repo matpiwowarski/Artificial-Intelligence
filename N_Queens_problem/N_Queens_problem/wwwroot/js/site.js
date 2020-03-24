1d8e794068b58950831ed0ab4e93d13d6b3cac00
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-24 14:31:42 +0100

    script to hide parameters

--- N_Queens_problem/N_Queens_problem/wwwroot/js/site.js
+++ N_Queens_problem/N_Queens_problem/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function selectAlgorithm() {
+
+    if (document.getElementById('AlgorithmSelect').value == 'Hill Climbing') {
+        document.getElementById('HillClimbingParameters').style.display = 'block';
+        document.getElementById('SimulatedAnnealingParameters').style.display = 'none';
+        document.getElementById('LocalBeamSearchParameters').style.display = 'none';
+        document.getElementById('GeneticParameters').style.display = 'none';
+    }
+    else if (document.getElementById('AlgorithmSelect').value == 'Simulated Annealing') {
+        document.getElementById('HillClimbingParameters').style.display = 'none';
+        document.getElementById('SimulatedAnnealingParameters').style.display = 'block';
+        document.getElementById('LocalBeamSearchParameters').style.display = 'none';
+        document.getElementById('GeneticParameters').style.display = 'none';
+    }
+    else if (document.getElementById('AlgorithmSelect').value == 'Local Beam Search') {
+        document.getElementById('HillClimbingParameters').style.display = 'none';
+        document.getElementById('SimulatedAnnealingParameters').style.display = 'none';
+        document.getElementById('LocalBeamSearchParameters').style.display = 'block';
+        document.getElementById('GeneticParameters').style.display = 'none';
+    }
+    else if (document.getElementById('AlgorithmSelect').value == 'Genetic') {
+        document.getElementById('HillClimbingParameters').style.display = 'none';
+        document.getElementById('SimulatedAnnealingParameters').style.display = 'none';
+        document.getElementById('LocalBeamSearchParameters').style.display = 'none';
+        document.getElementById('GeneticParameters').style.display = 'block';
+    }
+}
