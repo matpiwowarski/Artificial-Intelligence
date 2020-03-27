57563790e644396886bbf8c05cbfbea3b870b560
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-27 21:14:37 +0100

    LocalBeamSearchAlgorithm

--- N_Queens_problem/N_Queens_problem/Models/Algorithms/LocalBeamSearchAlgorithm.cs
+++ N_Queens_problem/N_Queens_problem/Models/Algorithms/LocalBeamSearchAlgorithm.cs
@@ -21,31 +21,79 @@ namespace N_Queens_problem.Models.Algorithms
                 states.Add(generatedState);
             }
 
+            var bestState = states[0];
+            int bestResult = Heuristic(states[0], size);
+
             // iterations - steps = depth of search
             for (int i = 0; i < maxNumberOfSteps; i++)
             {
-                var solvedState = ReturnStateIfAnyIsSolved(states, size);
+                MoveQueensInEveryState(states, size); // if state gets stuck => it will be replaced
 
-                if (solvedState != null)
-                {
-                    chessBoard.Board = solvedState;
-                    chessBoard.HeuristicResult = Heuristic(solvedState, size);
+                bestState = ReturnBestState(states, size);
+                bestResult = Heuristic(bestState, size);
+
+                if (bestResult == 0)
                     break;
+            }
+
+            chessBoard.Board = bestState;
+            chessBoard.HeuristicResult = bestResult;
+        }
+
+        private ChessPiece[,] ReturnBestState(List<ChessPiece[,]> states, int size)
+        {
+            int bestResult = Heuristic(states[0], size);
+            ChessPiece[,] bestState = new ChessPiece[size, size];
+
+            foreach (var state in states)
+            {
+                int newResult = Heuristic(state, size);
+
+                if (newResult < bestResult)
+                    bestState = state;
+            }
+
+            return bestState;
+        }
+
+        private void MoveQueensInEveryState(List<ChessPiece[,]> states, int size)
+        {
+            for(int index = 0; index < size; index++)
+            {
+                int resultBeforeChanges = Heuristic(states[index], size);
+
+                if (resultBeforeChanges == 0) // one of our state is solved so we don't care about the rest
+                    break;
+
+                for (int i = 0; i < size; i++) // every column
+                {
+                    for (int j = 0; j < size; j++) // checking which row is the best in 'i' column and we are moving there our queen
+                    {
+                        int rowBeforeMovingQueen = GetQueenRowInColumn(states[index], size, i);
+                        int resultBeforeMovingQueen = Heuristic(states[index], size);
+
+                        this.MoveQueenVertical(states[index], size, i, j);
+                        int newResult = this.Heuristic(states[index], size);
+
+                        if (newResult < resultBeforeMovingQueen)
+                        {
+                            // we have already moved queen so it stays
+                        }
+                        else // worse or equal => puting back our queen
+                        {
+                            this.MoveQueenVertical(states[index], size, i, rowBeforeMovingQueen);
+                        }
+
+                        if (newResult == 0) // h(x) == 0 => we solved the problem
+                            break;
+                    }
+                }
+                int resultAfterChanges = Heuristic(states[index], size);
+                if (resultBeforeChanges == resultAfterChanges) // we didn't move any queen => we generate new board
+                {
+                    states[index] = GenerateRandomBoardState(size);
                 }
             }
         }
-
-        private ChessPiece[,] ReturnStateIfAnyIsSolved(List<ChessPiece[,]> states, int size)
-        {
-            foreach(var state in states)
-            {
-                if (Heuristic(state, size) == 0)
-                    return state;
-            }
-
-            // there's no solution in list of states
-            return null;
-        }
-
     }
 }
