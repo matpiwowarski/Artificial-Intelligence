c375614d88abd54998aca974855556e290a1e59a
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-28 21:48:54 +0100

    beginning of genetic algorithm

--- N_Queens_problem/N_Queens_problem/Models/NQueensProblem/Algorithms/GeneticAlgorithm.cs
+++ N_Queens_problem/N_Queens_problem/Models/NQueensProblem/Algorithms/GeneticAlgorithm.cs
@@ -1,16 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace N_Queens_problem.Models.Algorithms
 {
     public class GeneticAlgorithm: Algorithm
     {
-        public override void SolveProblem(Chessboard chessboard)
+        public override void SolveProblem(Chessboard chessBoard)
         {
-            var sizeOfSingleGeneration = chessboard.Parameters.SizeOfSingleGeneration;
-            var percentOfElitism = chessboard.Parameters.PercentOfElitism / 100;
-            var crossoverProbability = chessboard.Parameters.CrossoverProbability / 100;
-            var mutationProbability = chessboard.Parameters.MutationProbability / 100;
-            var numberOfGenerations = chessboard.Parameters.NumberOfGenerations;
+            var sizeOfSingleGeneration = chessBoard.Parameters.SizeOfSingleGeneration;
+            var percentOfElitism = chessBoard.Parameters.PercentOfElitism / 100;
+            var crossoverProbability = chessBoard.Parameters.CrossoverProbability / 100;
+            var mutationProbability = chessBoard.Parameters.MutationProbability / 100;
+            var numberOfGenerations = chessBoard.Parameters.NumberOfGenerations;
 
+            var boardSize = chessBoard.Size;
+            int steps = 0;
+
+
+            var generation = new List<ChessPiece[,]>(); // generation -> list of states
+
+            // generate initial generation
+            for (int i = 0; i < sizeOfSingleGeneration; i++)
+            {
+                var generatedState = this.GenerateRandomBoardState(boardSize);
+                generation.Add(generatedState);
+            }
+
+            for(int i = 0; i < numberOfGenerations; i++)
+            {
+
+            }
+
+            var bestState = generation[0];
+            int bestResult = Heuristic(generation[0], boardSize);
+
+
+            chessBoard.Steps = steps;
+            chessBoard.Board = bestState;
+            chessBoard.HeuristicResult = bestResult;
         }
     }
 }
