95ce64201abb28260ca95ec1382c88442516ae96
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-03-28 16:03:13 +0100

    initial genetic parameters

---
+++ N_Queens_problem/N_Queens_problem/Models/NQueensProblem/Algorithms/GeneticAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace N_Queens_problem.Models.Algorithms
+{
+    public class GeneticAlgorithm: Algorithm
+    {
+        public override void SolveProblem(Chessboard chessboard)
+        {
+            var sizeOfSingleGeneration = chessboard.Parameters.SizeOfSingleGeneration;
+            var percentOfElitism = chessboard.Parameters.PercentOfElitism / 100;
+            var crossoverProbability = chessboard.Parameters.CrossoverProbability / 100;
+            var mutationProbability = chessboard.Parameters.MutationProbability / 100;
+            var numberOfGenerations = chessboard.Parameters.NumberOfGenerations;
+
+        }
+    }
+}
--- N_Queens_problem/N_Queens_problem/Models/NQueensProblem/Parameters.cs
+++ N_Queens_problem/N_Queens_problem/Models/NQueensProblem/Parameters.cs
@@ -12,9 +12,9 @@ namespace N_Queens_problem.Models
         public uint NumberOfStates { get; set; }
         // 4th algorithm
         public int SizeOfSingleGeneration { get; set; }
-        public int PercentOfElitism { get; set; }
-        public int CrossoverProbability { get; set; }
-        public int MutationProbability { get; set; }
+        public double PercentOfElitism { get; set; }
+        public double CrossoverProbability { get; set; }
+        public double MutationProbability { get; set; }
         public int NumberOfGenerations { get; set; }
 
         public Parameters()
@@ -24,6 +24,11 @@ namespace N_Queens_problem.Models
             StartingTemperature = 10000;
             CoolingFactor = 1;
             NumberOfStates = 20;
+            SizeOfSingleGeneration = 100;
+            PercentOfElitism = 20;
+            CrossoverProbability = 35;
+            MutationProbability = 5;
+            NumberOfGenerations = 1000;
         }
     }
 }
