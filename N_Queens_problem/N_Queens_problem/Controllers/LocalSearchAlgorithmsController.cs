9ebf94e7eab3b423c8fbfd0e49eefb413bef13da
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-04-08 12:59:29 +0200

    Local Search Algorithms into new controller

---
+++ N_Queens_problem/N_Queens_problem/Controllers/LocalSearchAlgorithmsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using N_Queens_problem.Models;
+using N_Queens_problem.Models.Algorithms;
+
+namespace ArtificialIntelligence.Controllers
+{
+    public class LocalSearchAlgorithmsController : Controller
+    {
+        public IActionResult Index(int size = 4)
+        {
+            NQueensProblem nQueensProblem = new NQueensProblem(size);
+
+            nQueensProblem.SetRandomBoardState();
+            nQueensProblem.DoAlgorithm(); // just heuristic result
+
+            var board = nQueensProblem.GetResultBoard();
+
+            board.CheckIfProblemSolved();
+
+            return View(board);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeBoardgameSize(IFormCollection formCollection)
+        {
+            int size = int.Parse(formCollection["SizeSelect"]);
+
+            NQueensProblem nQueensProblem = new NQueensProblem(size);
+
+            nQueensProblem.SetRandomBoardState();
+            nQueensProblem.DoAlgorithm(); // just heuristic result
+
+            var board = nQueensProblem.GetResultBoard();
+            board.CheckIfProblemSolved();
+
+            return View("Index", board);
+        }
+
+        [HttpPost]
+        public IActionResult DoAlgorithm(IFormCollection formCollection)
+        {
+            NQueensProblem nQueensProblem = new NQueensProblem();
+
+            var algorithmName = formCollection["AlgorithmSelect"];
+
+            switch (algorithmName)
+            {
+                case "Hill Climbing":
+                    try // taking parameters from View and set it
+                    {
+                        var maximumNumberOfSteps = uint.Parse(formCollection["MaxSteps"]);
+                        nQueensProblem.SetMaximumNumberOfSteps(maximumNumberOfSteps);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception message: {0}", e.Message);
+                    }
+                    nQueensProblem.SetAlgorithm(new HillClimbingAlgorithm());
+                    break;
+
+                case "Simulated Annealing":
+                    try // taking parameters from View and set it
+                    {
+                        var startingTemperature = int.Parse(formCollection["StartingTemperature"]);
+                        nQueensProblem.SetStartingTemperature(startingTemperature);
+                        var coolingFactor = int.Parse(formCollection["CoolingFactor"]);
+                        nQueensProblem.SetCoolingFactor(coolingFactor);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception message: {0}", e.Message);
+                    }
+                    nQueensProblem.SetAlgorithm(new SimulatedAnnealingAlgorithm());
+                    break;
+
+                case "Local Beam Search":
+                    try // taking parameters from View and set it
+                    {
+                        var maximumNumberOfSteps = uint.Parse(formCollection["MaxSteps"]);
+                        nQueensProblem.SetMaximumNumberOfSteps(maximumNumberOfSteps);
+                        var numberOfStates = uint.Parse(formCollection["NumberOfStates"]);
+                        nQueensProblem.SetNumberOfStates(numberOfStates);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception message: {0}", e.Message);
+                    }
+                    nQueensProblem.SetAlgorithm(new LocalBeamSearchAlgorithm());
+                    break;
+                case "Genetic":
+                    try // taking parameters from View and set it
+                    {
+                        var sizeOfSingleGeneration = int.Parse(formCollection["SizeOfSingleGeneration"]);
+                        nQueensProblem.SetSizeOfSingleGeneration(sizeOfSingleGeneration);
+
+                        var percentOfElitism = int.Parse(formCollection["PercentOfElitism"]);
+                        nQueensProblem.SetPercentOfElitism(percentOfElitism);
+
+                        var crossoverProbability = int.Parse(formCollection["CrossoverProbability"]);
+                        nQueensProblem.SetCrossoverProbability(crossoverProbability);
+
+                        var mutationProbability = int.Parse(formCollection["MutationProbability"]);
+                        nQueensProblem.SetMutationProbability(mutationProbability);
+
+                        var numberOfGenerations = int.Parse(formCollection["NumberOfGenerations"]);
+                        nQueensProblem.SetNumberOfGenerations(numberOfGenerations);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception message: {0}", e.Message);
+                    }
+                    nQueensProblem.SetAlgorithm(new GeneticAlgorithm());
+                    break;
+                default:
+                    nQueensProblem.SetAlgorithm(new HillClimbingAlgorithm());
+                    break;
+            }
+
+            nQueensProblem.DoAlgorithm();
+
+            var resultBoard = nQueensProblem.GetResultBoard();
+            resultBoard.CheckIfProblemSolved();
+
+            return View("Index", resultBoard);
+        }
+    }
+}
+
